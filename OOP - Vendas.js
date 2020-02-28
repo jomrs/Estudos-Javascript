6381c787f07ddc3fb98b09566be66dc4172a9ce4
Jonhtan Mota <jmtrs@rede.ulbra.br>
2020-02-28 09:53:48 -0300

    Ajustado ao ES6.

--- OOP - Vendas.js
+++ OOP - Vendas.js
@@ -1,37 +1,36 @@
-//Classes são funções
-function Produto (nomeP, descP, precoP) {
-    this.nome = nomeP;
-    this.desc = descP;
-    this.preco = precoP;
+//Classes em ES6.
+class Produto {
+    constructor(nomeP, descP, precoP) {
+        this.nome = nomeP;
+        this.desc = descP;
+        this.preco = precoP;
+    }
+    //Metódos em classe as ES6.
+    exibeProduto() {
+        console.log("Nome: " + this.nome + "\nDescrição:" + this.desc + "\n$:" + this.preco);
+    }
 };
 
-//métodos são prototipos
-Produto.prototype.exibeProduto = function () {
-    console.log("Nome: " + this.nome + "\nDescrição:" + this.desc + "\n$:" + this.preco);
-};
 
-//Instancia como um objeto.
+//Instancia a classe como um objeto.
 let fruta = new Produto("Morango", "Morangos vindo do Sul do país, fresquinhos!", 4.58);
 
 //Chama o método como propriedade.
 fruta.exibeProduto();
 
-//Herança
-function ProdutoExclusivo(nomeP, descP, precoP) {
-    //chama o pai, passando os atributos.
-    Produto.call(this, nomeP, descP, precoP);
+//Herança declaramos um extend para com a classe Pai.
+class ProdutoExclusivo extends Produto {
+    //No construtor invocamos o construtor do Pai com o super.
+    constructor(nomeP, descP, precoP){
+        super(nomeP, descP, precoP);
+    }
+    //Método do Pai sobrescrevido.
+    exibeProduto() {
+        console.log("[Produto Em Promoção]\nNome: " + this.nome + "\nDescrição:" + this.desc + "\n$:" + this.preco);
+    }
 }
-//Herda o pai.
-ProdutoExclusivo.prototype = new Produto();
-//Arrumando o ponteiro do construtor.
-ProdutoExclusivo.prototype.constructor = ProdutoExclusivo;
 
-//Sobrescrevendo o método do pai.
-ProdutoExclusivo.prototype.exibeProduto = function () {
-    console.log("[Produto Em Promoção]\nNome: " + this.nome + "\nDescrição:" + this.desc + "\n$:" + this.preco);
-};
-
-//Nova Instância.
+//Nova Instância agora como ProdutoExclusivo.
 let fruta2 = new ProdutoExclusivo("Laranja", "Laranja em promoção!", 3.25);
 
 //checar se é uma instância.
